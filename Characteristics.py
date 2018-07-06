dc72b4f3937c8b8f310fa8c9bd86e5561ef5592d
jasming1 <40004548+jasming1@users.noreply.github.com>
2018-07-06 13:19:47 -0400

    Update Characteristics.py

--- Characteristics.py
+++ Characteristics.py
@@ -3,9 +3,8 @@ from pyspark import SparkContext,SparkConf,StorageLevel
 from operator import add
 
 def neighborhood(graph, N, k):
-    """takes rdd of edges in graph and a maximum distance integer k, and returns a list of
-        tuples consisting of the node label and a list of the number of neighbors
-        at a distance from 1 to k"""
+    """takes rdd of edges in graph and a maximum distance integer k, and returns the number of neighbors
+        at a distance from 1 to k for each node"""
 
     # calculates the number of neighbors in the 1st neighborhood
     neighborhood1 = graph.mapValues(lambda node2: 1).reduceByKey(add)
@@ -18,44 +17,33 @@ def neighborhood(graph, N, k):
     all_neighborhoods = all_neighborhoods.leftOuterJoin(neighborhood1) \
         .mapValues(lambda (list, val): list + [val])
 
-    if k > 1:
-        # keeps track of all the edges found in neighborhoods so far
-        found_edges = graph.map(lambda edge: (edge, 1))
+    # keeps track of all the visited nodes for each node so far
+    visited_nodes = graph.mapValues(lambda x: {x}).reduceByKey(lambda x, y: x.union(y)).cache()
 
-        # edges in current k neighborhood
-        # this is initialized to the rdd of edges at distance 2
-        # look in for loop comments for explanation of filtering
-        current_k_edges = graph.join(graph).values().distinct() \
-            .map(lambda edge: (edge, 1)).leftOuterJoin(found_edges) \
-            .filter(lambda (edge, count): count[1] == None) \
-            .map(lambda (edge, count): edge).filter(lambda (n1, n2): n1 != n2)
+    # edges in current k neighborhood
+    # this is initialized to the rdd of edges at distance 2
+    current_k_edges = graph
 
+    for i in range(2, k + 1):
+        # finds edges at distance k and filters out nodes that were already found to be in
+        # other neighborhoods
+        current_k_edges = current_k_edges.join(graph).values().distinct() \
+            .filter(lambda (n1, n2): n1 != n2).partitionBy(N)\
+            .mapValues(lambda x: {x})\
+            .reduceByKey(lambda x, y: x.union(y))\
+            .leftOuterJoin(visited_nodes)\
+            .mapValues(lambda (set_current_k, set_visited_nodes): set_current_k - set_visited_nodes)\
+            .flatMapValues(lambda x: x).cache()
 
-        # calculates the number of neighbors in the 2nd neighborhood
-        neighborhood2 = current_k_edges.mapValues(lambda node2: 1) \
+        k_neighbors = current_k_edges.mapValues(lambda node2: 1) \
             .reduceByKey(add)
 
-        all_neighborhoods = all_neighborhoods.leftOuterJoin(neighborhood2) \
-            .mapValues(lambda (list, val): list + [val])
+        all_neighborhoods = all_neighborhoods.leftOuterJoin(k_neighbors) \
+            .mapValues(lambda (list, val): list + [val]).cache()
 
-        if k > 2:
-            # starts iterating from k = 3
-            for i in range(3, k + 1):
-                # updates found edges
-                found_edges = found_edges.union(current_k_edges.map(lambda edge: (edge, 1)))
-
-                # finds edges at distance k and filters out edges that were already found to be in
-                # other neighborhoods
-                current_k_edges = current_k_edges.join(graph).values().distinct() \
-                    .map(lambda edge: (edge, 1)).leftOuterJoin(found_edges) \
-                    .filter(lambda (edge, count): count[1] == None) \
-                    .map(lambda (edge, count): edge)
-
-                k_neighbors = current_k_edges.mapValues(lambda node2: 1) \
-                    .reduceByKey(add)
-
-                all_neighborhoods = all_neighborhoods.leftOuterJoin(k_neighbors) \
-                    .mapValues(lambda (list, val): list + [val])
+        # updates visited nodes
+        visited_nodes = current_k_edges.mapValues(lambda x: {x}).union(visited_nodes)\
+            .reduceByKey(lambda set1, set2: set1.union(set2)).cache()
 
     return all_neighborhoods
 
@@ -66,56 +54,34 @@ def update(d,key,val):
     return d
 
 def paths_and_cycles(graph, N, k):
-    """calculates the paths of length 1 to k and the cycles of length 3 to k"""
+    """takes rdd of edges in graph and a maximum distance integer k, and calculates the paths and cycles of length 1 to k for each node"""
 
-    # initializes an empty list of the node and the number of neighbors in each neighborhood from 1 to k
+    # initializes an empty list of the node and the number of cycles and paths in each neighborhood from 1 to k
     nodes = graph.flatMap(lambda (u, v): [u, v]).distinct()
     all_cycles_and_paths = nodes \
         .map(lambda u: (u, {'cycles': [], 'paths': []})) \
         .partitionBy(N).cache()
 
-    # keeps track of all the paths
-    current_paths = graph.map(lambda (x, y): (x, [x, y]))
+    # initializes the current paths to be the graph
+    current_paths = graph
 
-    # calculates the number of paths and updates the dictionary
-    paths = current_paths.mapValues(lambda path_list:1).reduceByKey(lambda x,y:x+y)
-    all_cycles_and_paths = all_cycles_and_paths \
-        .leftOuterJoin(paths) \
-        .mapValues(lambda (d, val): update(d, 'paths', val))\
-        .cache()
+    i = 0
+    while i < k:
+        # calculates the number of paths and cycles
+        paths = current_paths.mapValues(lambda n:1).reduceByKey(add)
 
-    #adds in paths of length 2
-    current_paths = current_paths.leftOuterJoin(graph) \
-        .map(lambda (n1, (path_list, n2)): (n2, [n2] + path_list))\
-        .filter(lambda (n1, path_list): path_list[0] != path_list[-1])\
-        .cache()
-
-    paths = current_paths.mapValues(lambda path_list: 1).reduceByKey(lambda x, y: x + y)
-    all_cycles_and_paths = all_cycles_and_paths \
-        .leftOuterJoin(paths) \
-        .mapValues(lambda (d, val): update(d, 'paths', val)) \
-        .cache()
-
-    # starts with k = 3
-    i=2
-    while i<k:
-        current_paths = current_paths.leftOuterJoin(graph)\
-            .map(lambda (n1, (path_list, n2)): (n2, [n2] + path_list))\
-            .filter(lambda (n1, path_list): path_list[0] not in path_list[1:-1])\
-            .cache()
-
-        paths = current_paths.mapValues(lambda path_list: 1).reduceByKey(lambda x, y: x + y)
-
-        # calculates the number of cycles
-        cycles = current_paths.filter(lambda (n1, path_list): path_list[0] == path_list[-1])\
-            .mapValues(lambda x: 1).reduceByKey(lambda x, y: x + y).mapValues(lambda x: x/2)
+        cycles = current_paths.filter(lambda (n1, n2): n1 == n2).mapValues(lambda n: 1)\
+            .reduceByKey(add)
 
         all_cycles_and_paths = all_cycles_and_paths \
             .leftOuterJoin(paths) \
-            .mapValues(lambda (d, val): update(d, 'paths', val))\
-            .leftOuterJoin(cycles)\
-            .mapValues(lambda (d, val): update(d, "cycles", val))\
+            .mapValues(lambda (d, val): update(d, 'paths', val)) \
+            .leftOuterJoin(cycles) \
+            .mapValues(lambda (d, val): update(d, 'cycles', val)) \
             .cache()
+
+        # update the current paths
+        current_paths = current_paths.leftOuterJoin(graph).values().cache()
 
         i += 1
 
@@ -141,10 +107,10 @@ if __name__ == "__main__":
     sc.setLogLevel("ERROR")
 
     lines = sc.textFile(args.graph)
-    graph = lines.map(eval)
-    #neighbors = neighborhood(graph, args.N, args.k).collect()
+    graph = lines.map(eval).partitionBy(args.N).cache()
 
-    #print(neighbors)
+    neighbors = neighborhood(graph, args.N, args.k).collect()
+    print(neighbors)
 
     cycle_paths = paths_and_cycles(graph, args.N, args.k).collect()
     print(cycle_paths)

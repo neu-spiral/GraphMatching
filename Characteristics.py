c73d69053d091819b5d76a8a371d518dcea739e6
Gao <jasming@discovery4.neu.edu>
2018-07-24 10:26:43 -0400

    accomadates more attributes

--- Characteristics.py
+++ Characteristics.py
@@ -1,7 +1,9 @@
 import sys,argparse
+import numpy as np
 from pyspark import SparkContext,SparkConf,StorageLevel
 from operator import add
 from helpers import safeWrite
+from preprocessor import readSnap
 
 def neighborhood(graph, N, k):
     """takes rdd of edges in graph and a maximum distance integer k, and returns the number of neighbors
@@ -60,6 +62,7 @@ def paths_and_cycles(graph, N, k):
 
     # initializes an empty list of the node and the number of cycles and paths in each neighborhood from 1 to k
     nodes = graph.flatMap(lambda (u, v): [u, v]).distinct()
+    number_of_nodes = float(nodes.count())
     all_cycles_and_paths = nodes \
         .map(lambda u: (u, {'cycles': [], 'paths': []})) \
         .partitionBy(N).cache()
@@ -70,6 +73,7 @@ def paths_and_cycles(graph, N, k):
     i = 0
     while i < k:
         # calculates the number of paths and cycles
+        checkpnt = i!=0 and i%4==0
         paths = current_paths.mapValues(lambda n:1).reduceByKey(add)
 
         cycles = current_paths.filter(lambda (n1, n2): n1 == n2).mapValues(lambda n: 1)\
@@ -81,41 +85,115 @@ def paths_and_cycles(graph, N, k):
             .leftOuterJoin(cycles) \
             .mapValues(lambda (d, val): update(d, 'cycles', val)) \
             .cache()
-
+        print "Avg. number of paths and cycles are", all_cycles_and_paths.values()\
+                                                    .map(lambda d: tuple([d[key] for key in d]))\
+                                                    .map(lambda (paths_list, cycles_list):(np.array(paths_list)/number_of_nodes, np.array(cycles_list)/number_of_nodes))\
+                                                    .reduce(lambda (paths_array1, cycles_array1), (paths_array2, cycles_array2):(paths_array1+paths_array2,cycles_array1+cycles_array2))
+        if checkpnt:
+            all_cycles_and_paths.checkpoint()
         # update the current paths
         current_paths = current_paths.leftOuterJoin(graph).values().cache()
-
+        if checkpnt:
+            current_paths.checkpoint()
         i += 1
 
     return all_cycles_and_paths
 
 
+def get_page_rank(graph, N, eps, max_iterations, gamma):
+    """Calculates the page rank for each node"""
+    graph_rdd = graph.groupByKey() \
+        .mapValues(list) \
+        .partitionBy(N) \
+        .cache()
+
+    # Discover all nodes; this finds node with no outgoing edges as well
+    nodes = graph_rdd.flatMap(lambda (i, edgelist): edgelist + [i]) \
+        .distinct() \
+        .cache()
+
+    # Initialize scores
+    size = nodes.count()
+    scores = nodes.map(lambda i: (i, 1.0 / size)).partitionBy(N).cache()
+
+    # Main iterations
+    i = 0
+    err = eps + 1.0
+    while i < max_iterations and err > eps:
+        i += 1
+        old_scores = scores
+        joined = graph_rdd.join(scores)
+        scores = joined.values() \
+            .flatMap(lambda (neighborlist, score): [(x, 1.0 * score / len(neighborlist)) for x in neighborlist]) \
+            .reduceByKey(lambda x, y: x + y, numPartitions=N) \
+            .mapValues(lambda x: (1 - gamma) * x + gamma * 1 / size) \
+            .cache()
+
+        err = old_scores.join(scores).values() \
+            .map(lambda (old_val, new_val): abs(old_val - new_val)) \
+            .reduce(lambda x, y: x + y)
+
+        old_scores.unpersist()
+        print
+        '### Iteration:', i, '\terror:', err
+
+        # Give score to nodes having no incoming edges. All such nodes
+    # should get score gamma / size
+    remaining_nodes = nodes.map(lambda x: (x, gamma / size)).subtractByKey(scores)
+    scores = scores.union(remaining_nodes)
+
+    return scores
+
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Finds number of neighbors in neighborhood k or the number '
+    parser = argparse.ArgumentParser(description = 'Finds number of neighbors in neighborhood k or the number '
                                                  'of cycles and paths of length k',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('mode', help = 'Option of what is being calculated (i.e. neighborhood size (NH), cycles and '
+                                     'paths (CP), page rank (PR).', choices = ['NH','CP', 'PR'])
     parser.add_argument('graph',
                         help ='Input Graph. The input should be a file containing one edge per line, '
                              'with each edge represented as a tuple of the form: (from_node,to_node).')
-    parser.add_argument('k', type=int,
-                        help ='Distance of largest path computed. E.g. k=2 computes the size of the '
+    parser.add_argument('k', type = int,
+                        help = 'Distance of largest path computed. E.g. k = 2 computes the size of the '
                              '2-degree neighborhood and the cycles and paths of length 2')
-    parser.add_argument('outputfile',type=str,help="The directory to save the results.")
-    parser.add_argument('--N',type=int, default=20, help = 'Level of Parallelism')
+    parser.add_argument('outputfile', type = str, help = "The directory to save the results.")
+    parser.add_argument('--N', type = int, default = 20, help = 'Level of Parallelism')
+    parser.add_argument('--gamma',type = float, default = 0.15, help = 'Interpolation parameter')
+    parser.add_argument('--max_iterations', type = int, default = 50, help = 'Maximum number of Iterations')
+    parser.add_argument('--eps', type = float, default = 0.01, help = 'Desired accuracy/epsilon value')
+    parser.add_argument('--fromSnap', action = 'store_true', help = 'Input file is from Snap')
+    parser.add_argument('--directed', action = 'store_true', help = 'Input file is directed')
 
     args = parser.parse_args()
 
     configuration = SparkConf()
     configuration.set('spark.default.parallelism', args.N)
-    sc = SparkContext(appName='Neighbors', conf=configuration)
+    sc = SparkContext(appName='Characteristics', conf=configuration)
     sc.setLogLevel("ERROR")
+    sc.setCheckpointDir("checkpointdir")
+    if args.fromSnap:
+        graph = readSnap(args.graph, sc, minPartitions = args.N).partitionBy(args.N).cache()
+        if args.directed:
+            graph = graph.flatMap(lambda (u, v): [(u, v), (v, u)]).partitionBy(args.N).cache()
 
-    lines = sc.textFile(args.graph)
-    graph = lines.map(eval).partitionBy(args.N).cache()
+    else:
+        lines = sc.textFile(args.graph)
+        graph = lines.map(eval).partitionBy(args.N).cache()
 
-    #neighbors = neighborhood(graph, args.N, args.k).collect()
-    #print(neighbors)
+    # returns the neighborhood sizes
+    if args.mode == 'NH':
+        neighbors = neighborhood(graph, args.N, args.k)
+        safeWrite(neighbors, args.outputfile)
 
-    cycle_paths = paths_and_cycles(graph, args.N, args.k)
-    safeWrite(cycle_paths, args.outputfile)
+    # returns the number of cycles and paths
+    if args.mode == 'CP':
+        cycle_paths = paths_and_cycles(graph, args.N, args.k)
+        safeWrite(cycle_paths, args.outputfile)
+
+    # returns the page rank
+    if args.mode == 'PR':
+        pageRank = get_page_rank(graph, args.N, args.eps, args.max_iterations, args.gamma)
+        pageRank.sortBy(lambda (key, val): -val)
+        safeWrite(pageRank, args.outputfile)
